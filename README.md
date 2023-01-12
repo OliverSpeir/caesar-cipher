e42ba5af503a3426d61704cf6ef8a182885a59d4
Oliver Speir <oliverspeir9@gmail.com>
2023-01-11 18:25:31 -0700

    lab18

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# caesar-cipher
+# LAB - Class 18
+
+## Project: Caesar Cipher
+
+### Author: Oliver Speir
+
+### Description
+
+create encrypt function that takes in a string and a number to shift by
+create decrypt function to do the same
+create crack function that brute force tries every possible shift
+
+### Setup
+
+- `pip install -r requirements.txt`
+### Setup
+
+- `python caesar_cipher.py`
+
+### Resources
+
+- https://www.rapidtables.com/code/text/ascii-table.html
---
+++ tests/test_caesar.py
@@ -0,0 +1,95 @@
+from caesar_cipher import encrypt, decrypt, crack
+
+
+def test_encrypt_shift_1():
+    actual = encrypt("apple", 1)
+    expected = "bqqmf"
+    assert actual == expected
+
+#
+def test_encrypt_shift_10():
+    actual = encrypt("apple", 10)
+    expected = "kzzvo"
+    assert actual == expected
+
+
+def test_encrypt_shift_20():
+    actual = encrypt("apple", 20)
+    expected = "ujjfy"
+    assert actual == expected
+
+
+def test_uppercase():
+    actual = encrypt("BANANA", 10)
+    expected = "LKXKXK"
+    assert actual == expected
+
+
+def test_with_whitespace():
+    actual = encrypt("apples and bananas", 1)
+    expected = "bqqmft boe cbobobt"
+    assert actual == expected
+
+
+def test_more_than_26_input():
+    actual = encrypt("apples and bananas", 53)
+    expected = "bqqmft boe cbobobt"
+    assert actual == expected
+
+
+def test_with_non_alpha():
+    actual = encrypt("Gimme a 1!", 1)
+    expected = "Hjnnf b 1!"
+    assert actual == expected
+#
+#
+def test_round_trip():
+    original = "Gimme a 1!"
+    shift = 5
+    encrypted = encrypt(original, shift)
+    actual = decrypt(encrypted, shift)
+    expected = original
+    assert actual == expected
+#
+#
+def test_round_trip_long():
+    original = "It was the best of times, it was the worst of times."
+    shift = 5
+    encrypted = encrypt(original, shift)
+    actual = decrypt(encrypted, shift)
+    expected = original
+    assert actual == expected
+#
+#
+def test_crack_phrase():
+    phrase = "It was the best of times, it was the worst of times."
+    encrypted = encrypt(phrase, 10)
+    actual = crack(encrypted)
+    expected = phrase
+    assert actual == expected
+
+
+def test_crack_nonsense():
+    phrase = "Ix fhw txe fofg of ndhrl, it nad tho hndrk of allkd."
+    encrypted = encrypt(phrase, 10)
+    actual = crack(encrypted)
+    expected = ""
+    assert actual == expected
+
+
+def test_crack_names():
+    phrase = "My name is Jason Daniel Christopher, son of Kelly and Judie Christopher."
+    encrypted = encrypt(phrase, 32)
+    print(encrypted)
+    actual = crack(encrypted)
+    expected = phrase
+    assert actual == expected
+
+
+def test_crack_mine():
+    phrase = "Cross the Rubicon"
+    encrypted = encrypt(phrase, 32)
+    print(encrypted)
+    actual = crack(encrypted)
+    expected = phrase
+    assert actual == expected
